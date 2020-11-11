181215b78795bae87cd2a7e1afeef33345dd10f4
HARRISKING <llc13588440410@outlook.com>
2020-11-11 09:33:56 +0800

    feat: 添加has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password
+end
+
+# user = User.new
+# user.email = '1@qq.com'
+# user.password = '123'
+# user.password_confirm = '123'
+# user.save
+def create
+  user = User.new
+
 end
